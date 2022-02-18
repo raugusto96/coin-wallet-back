b8cf2047cd5e56d2acda45a5f2677578828c521d
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-18 17:03:12 -0300

    Init server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`App listening the port: ${PORT}`));
