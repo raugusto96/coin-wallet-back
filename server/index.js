56cf22bf63f64e4e631614733ddc58289fefc58b
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-18 17:53:16 -0300

    Fix: resolve lint

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT;
+const { PORT } = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
