edbd7385d52be5de60f05656bcc41118e349ebb1
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-19 12:20:23 -0300

    Change index

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const { PORT } = process.env.PORT;
+const { PORT } = process.env;
 
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (_req, res) => {
+  res.status(200).json();
+});
+
 app.listen(PORT, () => console.log(`App listening the port: ${PORT}`));
+
+module.exports = app;
