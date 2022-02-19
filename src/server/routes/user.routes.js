1197384e17162e129842360dd1c875a946952bc6
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-19 19:45:56 -0300

    Adjust routes

--- src/server/routes/user.routes.js
+++ src/server/routes/user.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controllers = require('../controllers');
 
-router.post('/', controllers.user.logIn);
+router.post('/users', controllers.user.createUser);
+router.post('/login', controllers.user.logIn);
 
 module.exports = router;
