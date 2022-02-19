aa1002a906770f877151ecd836e8c07fb4281e7e
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-19 16:45:02 -0300

    Create connection model

---
+++ src/server/models/connection.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const { ATLAS_URI } = process.env;
+const { DB_NAME } = process.env;
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let db = null;
+
+const connection = () => (
+  db
+    ? Promise.resolve(db)
+    : MongoClient.connect(ATLAS_URI, OPTIONS)
+      .then((conn) => {
+        db = conn.db(DB_NAME);
+        return db;
+      })
+);
+
+module.exports = connection;
