07dbb053652eab895327d0d91da34ee3a5089830
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-19 19:22:59 -0300

    Create func createUser in user controller

--- src/server/controllers/user.controller.js
+++ src/server/controllers/user.controller.js
@@ -1,6 +1,11 @@
 const { StatusCodes } = require('http-status-codes');
 const services = require('../services');
 
+const createUser = async (req, res) => {
+  const user = await services.user.createUser(req.body);
+  return res.status(StatusCodes.OK).json({ user });
+};
+
 const logIn = async (req, res) => {
   const token = await services.user.logIn(req.body);
   if (token.message) return res.status(StatusCodes.NOT_ACCEPTABLE).json(token);
@@ -8,5 +13,6 @@ const logIn = async (req, res) => {
 };
 
 module.exports = {
+  createUser,
   logIn,
 };
