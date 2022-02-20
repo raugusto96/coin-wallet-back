3b94224945fdcae99a2256b66eeb237a19e0a26d
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-20 11:05:44 -0300

    Adjust validations

--- src/server/middlewares/validateUserInfos.middlewares.js
+++ src/server/middlewares/validateUserInfos.middlewares.js
@@ -1,19 +1,19 @@
 const validateName = (name) => {
-  if (name.length < 3) return { message: 'Name have must be more to 3 characters' };
+  if (name.length < 3) return { error: { message: 'Name have must be more to 3 characters' } };
   return true;
 };
 
 const validateEmail = (email) => {
   const emailRegex = /\S+@\S+\.\S+/;
   const isValidEmail = emailRegex.test(email);
-  if (isValidEmail) return { message: 'Email is not valid' };
+  if (!isValidEmail) return { error: { message: 'Email is not valid' } };
   return true;
 };
 
 const validatePassword = (password) => {
   const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
   const isValidPassword = passwordRegex.test(password);
-  if (isValidPassword) return { message: 'Password is not valid' };
+  if (!isValidPassword) return { error: { message: 'Password is not valid' } };
   return true;
 };
 
