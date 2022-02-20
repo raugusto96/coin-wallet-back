f743907d3cd00a33cf0055768ee5cacb641cdadd
Rodrigo <rodrigoaugusto96@outlook.com>
2022-02-20 10:19:43 -0300

    Create middleware validateEmail

--- src/server/middlewares/validateUserInfos.middlewares.js
+++ src/server/middlewares/validateUserInfos.middlewares.js
@@ -1,12 +1,16 @@
 const validateName = (name) => {
-  if (name.length < 3) {
-    return {
-      message: 'Name have must be more to 3 characters',
-    };
-  }
+  if (name.length < 3) return { message: 'Name have must be more to 3 characters' };
+  return true;
+};
+
+const validateEmail = (email) => {
+  const emailRegex = /\S+@\S+\.\S+/;
+  const isValidEmail = emailRegex.test(email);
+  if (isValidEmail) return { message: 'Email is not valid' };
   return true;
 };
 
 module.exports = {
   validateName,
+  validateEmail,
 };
