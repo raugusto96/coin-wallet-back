ec9e3dcd428519cf360be6849514b96185150809
Rodrigo <rodrigoaugusto96@outlook.com>
2022-03-18 08:44:15 -0300

    Fix: change the email regex

--- src/server/middlewares/validateUserInfos.middlewares.js
+++ src/server/middlewares/validateUserInfos.middlewares.js
@@ -6,7 +6,7 @@ const validateUser = (req, res, next) => {
   if (name.length < 3) {
     return res.status(StatusCodes.NOT_ACCEPTABLE).json(errorConstructor('"Name" have must be more to 3 characters'));
   }
-  const emailRegex = /\S+@\S+\.\S+/;
+  const emailRegex = /[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{1,4}\S$/;
   const isValidEmail = emailRegex.test(email);
   if (!isValidEmail) {
     return res.status(StatusCodes.NOT_ACCEPTABLE).json(errorConstructor('"Email" is not valid'));
