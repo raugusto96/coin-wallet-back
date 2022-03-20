f3d2307b67fa5e7f2c2b253f4848a55adc44eab6
Rodrigo <rodrigoaugusto96@outlook.com>
2022-03-20 09:25:28 -0300

    Fix: mail text

--- src/api/middlewares/nodemailer.middlewares.js
+++ src/api/middlewares/nodemailer.middlewares.js
@@ -19,13 +19,21 @@ module.exports = (req, _res, next) => {
     },
   });
 
+  const mailText = /* html */`
+  <p>Caro(a) <b>${name}</b>,<br>
+  você solicitou a recuperação de sua senha de acesso ao sistema do Coin Wallet.<br>
+  Clique no link abaixo para cadastrar a nova senha:<br>
+  <a href="http://localhost:3000/${email}/reset-password">Recuperar minha senha</a></p>
+  <p>Caso seu cliente de email não permita clicar no link acima, copie e cole o endereço abaixo no seu navegador de internet:<br>
+  http://localhost:3000/${email}/reset-password</p>
+  <h5>Favor não responder à este email. Essa é uma mensagem automática.</h5>
+  `;
+
   const mail = {
     from: MAIL_USERNAME,
     to: email,
-    subject: 'Coin Wallet Project',
-    html: `<b>Olá ${name}!</b><br>
-    Esse é um email para recuperar sua senha.<br>
-    <a href="http://teste.com">Link de teste, não clique</a>`,
+    subject: 'Coin Wallet - Recuperação de Senha',
+    html: mailText,
   };
 
   transporter.sendMail(mail, (error) => {
