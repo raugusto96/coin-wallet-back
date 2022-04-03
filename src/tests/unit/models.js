0b2a775dc8773b17ae14e3a2daecb9b1540e8dda
Rodrigo <rodrigoaugusto96@outlook.com>
2022-04-02 22:01:20 -0300

    User model login test

--- src/tests/unit/models.js
+++ src/tests/unit/models.js
@@ -3,6 +3,7 @@ const { ObjectId } = require('mongodb');
 require('dotenv').config();
 const { expect } = require('chai');
 const { MongoClient } = require('mongodb');
+const { set, reset } = require('mockdate');
 const errorConstructor = require('../../api/utils/errorConstructor.function');
 const { DB_NAME, FIRST_COLLECTION_NAME, SECOND_COLLECTION_NAME } = process.env;
 
@@ -14,17 +15,22 @@ const sampleCorrectUser = {
 	email: "email@mail.com",
 	name: "Fulano",
 	password: "123456789"
-}
+};
 
-const sampleIncorrectUser = {
-	email: "email@mailcom",
-	name: "Fu",
-	password: "123"
+const date = new Date();
+
+const expectedUser = {
+  _id: new ObjectId(),
+  email: "email@mail.com",
+  name: "Fulano",
+  password: "123456789",
+  created: date,
+  updated: date,
+  userId: 1
 }
 
 describe('Testando o user.model', () => {
   let connectionMock;
-
   before(async () => {
     connectionMock = await getConnection();
     sinon.stub(MongoClient, 'connect').resolves(connectionMock);
@@ -35,22 +41,28 @@ describe('Testando o user.model', () => {
     MongoClient.connect.restore();
   });
 
+  beforeEach(() => {
+    set(date);
+  });
+
+  afterEach(() => {
+    reset();
+  });
+
   describe('Testando o user.model.createUser', () => {
       it('Deve criar um usuário corretamente', async () => {
         const user = await models.user.createUser(FIRST_COLLECTION_NAME, sampleCorrectUser);
-
-        const expectedUser = {
-          _id: new ObjectId(),
-          email: "email@mail.com",
-          name: "Fulano",
-          password: "123456789",
-          created: new Date(),
-          updated: new Date(),
-          userId: 1
-        }
 
         expect(user).to.be.a('object');
         expect(user).to.deep.equal(expectedUser);
       });
   });
+  describe('Testando o user.model.logIn', () => {
+    it('Deve retornar o usuário cadastrado anteriormente', async () => {
+      const user = await models.user.logIn(FIRST_COLLECTION_NAME, sampleCorrectUser);
+
+      expect(user).to.be.a('object');
+      expect(user).to.deep.equal(expectedUser);
+    });
+  })
 });
