351f9ed0717212b1f56894645af9673eb561d2b8
Rodrigo <rodrigoaugusto96@outlook.com>
2022-05-08 13:19:10 -0300

    Fix: add middleware validate password in reset-pass route

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -7,6 +7,6 @@ router.post('/login', middlewares.login, controllers.user.logIn);
 router.get('/:id', middlewares.validateToken, controllers.user.findById);
 router.delete('/:id', middlewares.validateToken, controllers.user.deleteById);
 router.post('/reset-password', middlewares.email, controllers.user.sendEmail);
-router.put('/:email/reset-password', controllers.user.resetPassword);
+router.put('/:email/reset-password', middlewares.password,controllers.user.resetPassword);
 
 module.exports = router;
