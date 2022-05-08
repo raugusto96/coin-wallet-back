53e067f436e5ca42c9c98b7a0f654e62f9e7b125
Rodrigo <rodrigoaugusto96@outlook.com>
2022-05-08 18:33:07 -0300

    findByUserId model test

---
+++ src/tests/unit/models/findByUserId.model.test.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { MongoClient } = require('mongodb');
+const mock = require('../../mock');
+const models = require('../../../api/models');
+const { getConnection } = require('./mongoMockConnection');
+
+const { DB_NAME, FIRST_COLLECTION_NAME } = process.env;
+
+describe('Acha um usuário pelo id', () => {
+  let connectionMock;
+  
+  before(async () => {
+    connectionMock = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+    await connectionMock.db(DB_NAME).createCollection(FIRST_COLLECTION_NAME);
+    await models.user.createUser(FIRST_COLLECTION_NAME, mock.createUser.payloadCreateUser);
+  });
+  
+  after(async () => {
+    await connectionMock.db(DB_NAME).collection(FIRST_COLLECTION_NAME).drop();
+    MongoClient.connect.restore();
+  });
+
+  describe('Quando encontra um usuário corretamente', () => {
+    it('Retorna um objeto', async () => {
+      const id = 1;
+      const response = await models.user.findByUserId(FIRST_COLLECTION_NAME, id);
+      expect(response).to.be.an('object');
+    });
+    it('Retorna um objeto com as chaves "email", "name", "password", "userId", "id"', async () => {
+      const id = 1;
+      const response = await models.user.findByUserId(FIRST_COLLECTION_NAME, id);
+      expect(response).to.have.all.keys(['email', 'name', 'password', 'userId', '_id', 'created', 'updated']);
+    });
+  });
+});
