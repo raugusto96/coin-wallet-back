41e261b7c1f2b0e37d50cc58baeb28610589ccfc
Rodrigo <rodrigoaugusto96@outlook.com>
2022-05-08 16:24:29 -0300

    Feat: Add connection in index

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -1,7 +1,9 @@
 const user = require('./user.model');
 const expense = require('./expense.model');
+const connection = require('./connection');
 
 module.exports = {
   user,
   expense,
+  connection,
 };
