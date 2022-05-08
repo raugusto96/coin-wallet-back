b10e3b05a25c36fa918ed755ae6a764380e973cf
Rodrigo <rodrigoaugusto96@outlook.com>
2022-05-08 12:02:43 -0300

    Fix: updateExpense model

--- src/api/models/expense.model.js
+++ src/api/models/expense.model.js
@@ -10,13 +10,12 @@ const getAllExpenses = async (collectionName) => {
 
 const updateExpense = async (collectionName, { id, value, title, type, category }) => {
   const db = await connection();
-  const updated = await db.collection(collectionName)
+  await db.collection(collectionName)
     .updateOne({ _id: ObjectId(id) }, {
       $set: {
         value, title, type, category, updated: new Date(),
       },
     });
-  return updated;
 };
 
 const findById = async (collectionName, id) => {
