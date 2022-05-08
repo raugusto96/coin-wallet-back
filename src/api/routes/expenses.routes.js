7d41c26db3b4af0246fef93d54ca935d5237456e
Rodrigo <rodrigoaugusto96@outlook.com>
2022-05-08 11:11:32 -0300

    Fix: Add middleware validate expense put route

--- src/api/routes/expenses.routes.js
+++ src/api/routes/expenses.routes.js
@@ -8,8 +8,17 @@ router.post(
   middlewares.expense.validateExpenseData,
   controllers.expenses.createExpense,
 );
-router.get('/:userId', middlewares.validateToken, controllers.expenses.getAllExpensesByUser);
-router.put('/:id', middlewares.validateToken, controllers.expenses.updateById);
+router.get(
+  '/:userId',
+  middlewares.validateToken,
+  controllers.expenses.getAllExpensesByUser
+);
+router.put(
+  '/:id',
+  middlewares.validateToken,
+  middlewares.expense.validateExpenseData,
+  controllers.expenses.updateById
+);
 router.delete('/:id', middlewares.validateToken, controllers.expenses.deleteById);
 
 module.exports = router;
