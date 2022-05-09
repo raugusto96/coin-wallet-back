6319cc8b22fc38735328bc26a4834a58a5da8923
Rodrigo <rodrigoaugusto96@outlook.com>
2022-05-09 20:39:51 -0300

    Feat: findById service test

---
+++ src/tests/unit/services/findById.test.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const sinon = require('sinon');
+const { expect } = require('chai');
+const services = require('../../../api/services');
+const models = require('../../../api/models');
+const mock = require('../../mock');
+
+const { FIRST_COLLECTION_NAME } = process.env;
+
+describe('Encontra um usuário pelo id', () => {
+
+  before(() => {
+    sinon.stub(models.user, 'findByUserId').resolves(mock.user.responseFindedUser);
+  });
+
+  after(() => {
+    models.user.findByUserId.restore();
+  });
+
+  describe('Quando encontra um usuário', () => {
+    it('Retorna um objeto', async () => {
+      const response = await services.user.findById(FIRST_COLLECTION_NAME, '1');
+      expect(response).to.be.an('object');
+    });
+    it('Retorna um objeto com as chaves "email", "name", "userId"', async () => {
+      const response = await services.user.findById(FIRST_COLLECTION_NAME, '1');
+      expect(response).to.have.all.keys(['email', 'name', 'userId']);
+    });
+    it('Retorna um objeto com a chave "email" e o valor "mail@mail.com"', async () => {
+      const response = await services.user.findById(FIRST_COLLECTION_NAME, '1');
+      expect(response).to.have.a.property('email');
+      expect(response.email).to.be.equal('mail@mail.com');
+    });
+    it('Retorna um objeto com a chave "name" e o valor "Fulana da Silva"', async () => {
+      const response = await services.user.findById(FIRST_COLLECTION_NAME, '1');
+      expect(response).to.have.a.property('name');
+      expect(response.name).to.be.equal('Fulana da Silva');
+    });
+    it('Retorna um objeto com a chave "userId" e o valor "1"', async () => {
+      const response = await services.user.findById(FIRST_COLLECTION_NAME, '1');
+      expect(response).to.have.a.property('userId');
+      expect(response.userId).to.be.equal(1);
+    });
+  });
+  describe('Quando não encontra um usuário', () => {
+    it('Lança um erro', async () => {
+      models.user.findByUserId.restore();
+      sinon.stub(models.user, 'findByUserId').rejects('User doesn\'t exist');
+      try {
+        await services.user.findById(FIRST_COLLECTION_NAME, '2');        
+      } catch (error) {
+        expect(error).to.be.a('error');
+      }
+    });
+    it('Lança um erro com a mensagem "User doesn\'t exist"', async () => {
+      models.user.findByUserId.restore();
+      sinon.stub(models.user, 'findByUserId').rejects('User doesn\'t exist');
+      try {
+        await services.user.findById(FIRST_COLLECTION_NAME, '2');        
+      } catch (error) {
+        expect(error).to.match(/user doesn\'t exist/i);
+      }
+    });
+  });
+});
