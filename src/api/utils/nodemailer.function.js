b3aafd2024839e1672af25cc4587630c05014450
Rodrigo <rodrigoaugusto96@outlook.com>
2022-05-13 17:10:05 -0300

    Fix: change module.exports

--- src/api/utils/nodemailer.function.js
+++ src/api/utils/nodemailer.function.js
@@ -5,33 +5,35 @@ const {
   MAIL_USERNAME, MAIL_PASSWORD
 } = process.env;
 
-module.exports = (email) => {
-  const transporter = mailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: MAIL_USERNAME,
-      pass: MAIL_PASSWORD,
-    },
-  });
+module.exports = {
+  sendMail: (email) => {
+    const transporter = mailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: MAIL_USERNAME,
+        pass: MAIL_PASSWORD,
+      },
+    });
 
-  const mailText = /* html */`
-  Você solicitou a recuperação de sua senha de acesso ao sistema do Coin Wallet.<br>
-  Clique no link abaixo para cadastrar a nova senha:<br>
-  <a href="http://localhost:3000/${email}/reset-password">Recuperar minha senha</a></p>
-  <p>Caso seu cliente de email não permita clicar no link acima, copie e cole o endereço abaixo no seu navegador de internet:<br>
-  http://localhost:3000/${email}/reset-password</p>
-  <h5>Favor não responder à este email. Essa é uma mensagem automática.</h5>
-  `;
+    const mailText = /* html */`
+    Você solicitou a recuperação de sua senha de acesso ao sistema do Coin Wallet.<br>
+    Clique no link abaixo para cadastrar a nova senha:<br>
+    <a href="http://localhost:3000/${email}/reset-password">Recuperar minha senha</a></p>
+    <p>Caso seu cliente de email não permita clicar no link acima, copie e cole o endereço abaixo no seu navegador de internet:<br>
+    http://localhost:3000/${email}/reset-password</p>
+    <h5>Favor não responder à este email. Essa é uma mensagem automática.</h5>
+    `;
 
-  const mail = {
-    from: MAIL_USERNAME,
-    to: email,
-    subject: 'Coin Wallet - Recuperação de Senha',
-    html: mailText,
-  };
+    const mail = {
+      from: MAIL_USERNAME,
+      to: email,
+      subject: 'Coin Wallet - Recuperação de Senha',
+      html: mailText,
+    };
 
-  transporter.sendMail(mail, (error, info) => {
-    error ? next(error) : console.log(info.response);
-    transporter.close();
-  });
+    transporter.sendMail(mail, (error, info) => {
+      error ? next(error) : console.log(info.response);
+      transporter.close();
+    });
+  }
 };
