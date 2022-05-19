def9b26974ba9d887c097e7ac90b62bcd99fb32b
Rodrigo <rodrigoaugusto96@outlook.com>
2022-05-18 23:17:26 -0300

    Feat: create login controller test

---
+++ src/tests/unit/controllers/user/login.controller.test.js
@@ -0,0 +1,58 @@
+const { StatusCodes } = require('http-status-codes');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const services = require('../../../../api/services');
+const controllers = require('../../../../api/controllers');
+const mock = require('../../../mock');
+
+describe('Testa a função login do controller', () => {
+  
+  const request = {};
+  const response = {};
+  let next = null;
+
+  describe('Quando o usuário loga com sucesso', () => {
+
+    before(() => {
+      sinon.stub(services.user, 'logIn').resolves(mock.user.responseCreatedData);
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+    });
+
+    after(() => {
+      services.user.logIn.restore();
+    });
+
+    it('Envia um status "200"', async () => {
+      await controllers.user.logIn(request, response);
+
+      expect(response.status.calledWith(StatusCodes.OK)).to.be.equal(true);
+    });
+    it('Envia o usuário que conseguiu realizar o login', async () => {
+      await controllers.user.logIn(request, response);
+
+      expect(response.json.calledWith(mock.user.responseCreatedData)).to.be.equal(true);
+    });
+  });
+
+  describe('Quando não consegue realizar login', () => {
+    
+    before(() => {
+      sinon.stub(services.user, 'logIn').rejects({ message: 'User doesn\'t exist' });
+      next = sinon.stub().returns();
+    });
+    
+    after(() => {
+      services.user.logIn.restore();
+    });
+
+    it('Verifica se a função "next" recebe um erro como parametro', async () => {
+      try {
+        await controllers.user.logIn(request, response, next);
+      } catch (error) {
+        console.log(error);
+        expect(next.calledWith(error)).to.be.equal(true);
+      }
+    });
+  });
+});
