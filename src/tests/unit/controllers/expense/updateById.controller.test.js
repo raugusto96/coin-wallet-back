c6df89651c210bd3e0dddffafe2c04795a8ea2f1
Rodrigo <rodrigoaugusto96@outlook.com>
2022-05-22 09:24:48 -0300

    Feat: create updateById controller test

---
+++ src/tests/unit/controllers/expense/updateById.controller.test.js
@@ -0,0 +1,62 @@
+const { StatusCodes } = require('http-status-codes');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const services = require('../../../../api/services');
+const controllers = require('../../../../api/controllers');
+
+describe('Testando a função updateById do controller', () => {
+  
+  const request = {};
+  const response = {};
+  let next = () => {};
+
+  describe('Quando atualiza a despesa com sucesso', () => {
+    
+    before(() => {
+      sinon.stub(services.expenses, 'updateById').resolves();
+      request.params = { id: '1' };
+      request.body = {
+        value: 75,
+        title: 'Cinema',
+        category: 'Lazer',
+        type: 'withdraw',
+      };
+      response.status = sinon.stub().returns(response);
+      response.end = sinon.stub().returns();
+    });
+    
+    after(() => {
+      services.expenses.updateById.restore();
+    });
+
+    it('Envia um status "204"', async () => {
+      await controllers.expenses.updateById(request, response, next);
+
+      expect(response.status.calledWith(StatusCodes.NO_CONTENT)).to.be.equal(true);
+    });
+
+    it('Envia um usuário com sua despesa ao finalizar a requisição', async () => {
+      await controllers.expenses.updateById(request, response, next);
+
+      expect(response.end.called).to.be.equal(true);
+    });
+  });
+
+  describe('Quando não encontra a despesa', () => {
+    
+    before(() => {
+      sinon.stub(services.expenses, 'updateById').throws({ message: 'Expense does not exist' });
+      next = sinon.spy();
+    });
+
+    after(() => {
+      services.expenses.updateById.restore();
+    });
+
+    it('Verifica se a função "next" foi chamada', async () => {
+      await controllers.expenses.updateById(request, response, next);
+
+      expect(next.calledOnce).to.be.equal(true);
+    });
+  });
+});
